705f63d08301a81f790e3a5d9b093fd997f7f22c
Nishantha <nishanishagoo@gmail.com>
2026-02-13 01:51:35 +0530

    Added main.py and models.py

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from geoalchemy2 import Geometry # For handling location data
+from .database import Base
+import datetime
+
+class Report(Base):
+    __tablename__ = "reports"
+
+    id = Column(Integer, primary_key=True, index=True)
+    category = Column(String)
+    description = Column(String)
+    status = Column(String, default="unverified")
+    # This stores the lat/lng as a geometry point
+    location = Column(Geometry('POINT', srid=4326)) 
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
